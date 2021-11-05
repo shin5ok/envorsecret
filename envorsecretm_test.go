9c1a02d339e65e75649f61afe8d8407ff8d542ee
Shingo <kawanos@google.com>
2021-11-06 00:15:20 +0900

    do test

---
+++ envorsecretm_test.go
@@ -0,0 +1,17 @@
+package envorsecretm_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/shin5ok/envorsecretm"
+)
+
+func TestEnvsecretm(t *testing.T) {
+	ProjectId := os.Getenv("PROJECT_ID")
+	m := envorsecretm.Config{ProjectId}
+	if m.Get("PROJECT_ID") != ProjectId {
+		t.Logf("PROJECT_ID is NOT %s", ProjectId)
+	}
+
+}
